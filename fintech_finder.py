20eb91f6537eda18ef5b36730e7025ecfd4f7dd2
Alexander Valenzuela <111409358+AlexanderValenzuela@users.noreply.github.com>
2023-07-28 22:09:34 -0700

    update - 07_28_2023

--- fintech_finder.py
+++ fintech_finder.py
@@ -132,7 +132,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-account = generate_account()
+account = generate_account(w3)
 
 ##########################################
 
@@ -271,7 +271,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    transaction_hash = send_transaction(account, candidate_address, wage)
+    transaction_hash = send_transaction(w3,account, candidate_address, wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
